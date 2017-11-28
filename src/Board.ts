af69a9bc8f90fce1c4c4c651e8045017d509dcdf
icesheep <380131090@qq.com>
2017-11-28 15:19:23 +0800

    #棋子移动规则

--- src/Board.ts
+++ src/Board.ts
@@ -13,7 +13,8 @@ class Board extends egret.DisplayObjectContainer {
     private chooseFlag:boolean = false;
     private eat:boolean = false;
     private choose:Piece;
-    public pieceMap:{[key:string]:Piece} = {};
+    private rule:Rule = new Rule();
+    public static pieceMap:{[key:string]:Piece} = {};
     public static playerColor:number;
     public init(color:number,x:number,y:number,gridX:number,gridY:number)
     {
@@ -34,7 +35,7 @@ class Board extends egret.DisplayObjectContainer {
             p.transformToPosition(piece[i].x,piece[i].y,piece[i].name,Color.Red + Math.floor(i/16));
             p.bitMap = b;
             p.key = i;
-            this.pieceMap[i] = p;
+            Board.pieceMap[i] = p;
             b.name = piece[i].name;
             b.x = p.x;
             b.y = p.y;
@@ -51,67 +52,73 @@ class Board extends egret.DisplayObjectContainer {
     //棋盘点击事件
     private judge(evt:egret.TouchEvent)
     {
-        if(this.chooseFlag) {  //已经选中棋子
+        let temp:{pointX:number,pointY:number} = this.findPoint(evt.stageX,evt.$stageY);
+        if(!temp) {
+            return;
+        }
+        let p:Piece = this.findPiece(temp.pointX,temp.pointY);
+        if(this.chooseFlag) {  //第一次已经选中棋子
             console.log(evt,"judge");
-            if(true) { //能够移动
-                let temp:{pointX:number,pointY:number} = this.findPoint(evt.stageX,evt.$stageY);
-                let p:Piece = this.findPiece(temp.pointX,temp.pointY);
-                this.con.removeChildren(); //去掉选择框
-                if(p && p != this.choose) { //吃子
-                    this.choose.x = p.x;
-                    this.choose.y = p.y;
-                    this.choose.pointX = p.pointX;
-                    this.choose.pointY = p.pointY;
-                    this.choose.bitMap.x = p.x;
-                    this.choose.bitMap.y = p.y;
-                    delete this.pieceMap[p.key];
-                    this.chooseFlag = false;
-                    this.choose = null;
-                    this.removeChild(p.bitMap);
-                } else { //移动
-                    let tempx:number;
-                    let tempy:number;
-                    if(Board.playerColor === Color.Red) {
-                        tempx = Board.startX + (9-temp.pointX)*Board.gridX ;
-                        tempy = Board.startY + (10-temp.pointY)*Board.gridY ;
-                    } else {
-                        tempx = Board.startX + (temp.pointX-1)*Board.gridX ;
-                        tempy = Board.startY + (temp.pointY-1)*Board.gridY ;
+            if(p) { //第二次选中的棋子
+                if(p.color !== Board.playerColor) { //第二次选择的对面的棋子
+                    if(this.rule.ifLegal(this.choose.name,this.choose.pointX,this.choose.pointY,p.pointX,p.pointY)) { //能够移动
+                        this.con.removeChildren(); //去掉选择框
+                         //吃子
+                        this.choose.x = p.x;
+                        this.choose.y = p.y;
+                        this.choose.pointX = p.pointX;
+                        this.choose.pointY = p.pointY;
+                        this.choose.bitMap.x = p.x;
+                        this.choose.bitMap.y = p.y;
+                        egret.Tween.get(this.choose.bitMap).to( {x:p.x,y:p.y}, 2000, egret.Ease.sineIn );
+                        if(p.name === 'King') {
+                            alert(Board.playerColor+'获胜!');
+                        }
+                        delete Board.pieceMap[p.key];
+                        this.chooseFlag = false;
+                        this.choose = null;
+                        this.removeChild(p.bitMap);
                     }
+                } else { //第二次选择的自己的棋子，重新画框
+                    this.drawRect(p);
+                }
+            }else {  //第二次选择的空白区域
+                let tempx:number;
+                let tempy:number;
+                if(Board.playerColor === Color.Red) {
+                    tempx = Board.startX + (9-temp.pointX)*Board.gridX ;
+                    tempy = Board.startY + (10-temp.pointY)*Board.gridY ;
+                } else {
+                    tempx = Board.startX + (temp.pointX-1)*Board.gridX ;
+                    tempy = Board.startY + (temp.pointY-1)*Board.gridY ;
+                }
+                if(this.rule.ifLegal(this.choose.name,this.choose.pointX,this.choose.pointY,temp.pointX,temp.pointY)){  //可以移动
+                    this.con.removeChildren(); //去掉选择框               
                     this.choose.x = tempx;
                     this.choose.y = tempy;
                     this.choose.pointX = temp.pointX;
                     this.choose.pointY = temp.pointY;
                     this.choose.bitMap.x = tempx;
                     this.choose.bitMap.y = tempy;
+                    egret.Tween.get(this.choose.bitMap).to( {x:tempx,y:tempy}, 1000, egret.Ease.sineIn );
                     this.chooseFlag = false;
-                    this.choose = null;
+                    // this.choose = null;
                 }
             }
-        }else { //未选择棋子，进行选择
-            let temp:{pointX:number,pointY:number} = this.findPoint(evt.stageX,evt.$stageY);
-            let p:Piece = this.findPiece(temp.pointX,temp.pointY);
+        }else { //未选择棋子，进行第一次选择
             // console.log(p,"judge");
             if(p && p.color === Board.playerColor) {
-                this.cage.graphics.lineStyle(2, 0x00CD00, 1, true)
-                this.cage.graphics.drawRect(0,0,75,78);
-                this.cage.graphics.endFill();
-                this.con.addChild(this.cage);
-                this.con.x = p.x-37.5;
-                this.con.y = p.y-39;
-                this.chooseFlag = true;
-                this.choose = p;
+                this.drawRect(p);
             }
         }
     }
-    
+
     //根据棋盘坐标获取棋子
     private findPiece(pointX:number,pointY:number):Piece {
-        console.log(this.pieceMap,pointX,pointY);
-        for(let v in this.pieceMap){
-            console.log(this.pieceMap[v]);
-            if(this.pieceMap[v].pointX === pointX && this.pieceMap[v].pointY === pointY) {
-                return this.pieceMap[v];
+        console.log(Board.pieceMap,pointX,pointY);
+        for(let v in Board.pieceMap){
+            if(Board.pieceMap[v].pointX === pointX && Board.pieceMap[v].pointY === pointY) {
+                return Board.pieceMap[v];
             }
         }
         return null;
@@ -128,7 +135,24 @@ class Board extends egret.DisplayObjectContainer {
             pointX = Math.round((x - Board.startX)/Board.gridX) + 1;
             pointY = Math.round((y - Board.startY)/Board.gridY) + 1;
         }
-        return {pointX:pointX, pointY:pointY};
+        if(pointX>=1 && pointX <=9 && pointY>=1 && pointY<=10){
+            return {pointX:pointX, pointY:pointY};
+        }else{
+            return null;
+        }
+    }
+
+    //画选择框
+    private drawRect(p:Piece):void {
+        this.cage.graphics.lineStyle(2, 0x00CD00, 1, true)
+        this.cage.graphics.drawRect(0,0,75,78);
+        this.cage.graphics.endFill();
+        this.con.addChild(this.cage);
+        this.con.x = p.x-37.5;
+        this.con.y = p.y-39;
+        this.chooseFlag = true;
+        this.choose = p;
     }
     
+
 }
---
+++ src/Rule.ts
@@ -0,0 +1,175 @@
+
+class Rule {
+
+    public ifLegal(piece:string,startX:number,startY:number,endX:number,endY:number):boolean {
+        let a:{x:number,y:number,z:number,dx:number,dy:number,dz:number} = this.pieceCount(startX,startY,endX,endY);
+        console.log(a,piece);
+        switch(piece) {
+            case "Rook" : {
+                if(startX===endX && a.y === 0) {
+                    return true;
+                }else if(startY===endY && a.x === 0) {
+                    return true;
+                }else {
+                    return false;
+                }
+            }
+            case "Knight" : {
+                if(a.dx===1 && a.dy===2 && a.y===0) {
+                    return true;
+                }else if(a.dx===2 && a.dy===1 && a.x===0) {
+                    return true;
+                }else {
+                    return false;
+                }
+            }
+            case "Bishop" : {
+                if(Board.playerColor === Color.Red) {
+                    if(endY > 5) {
+                        return false;
+                    }
+                }else {
+                    if(endY < 6) {
+                        return false;
+                    }
+                }
+                if(a.dx===2 && a.dy===2 && a.z===0) {
+                    return true;
+                }else {
+                    return false;
+                }
+            }
+            case "Guard" : {
+                if(Board.playerColor === Color.Red) {
+                    if(endY > 3) {
+                        return false;
+                    }
+                }else {
+                    if(endY < 8) {
+                        return false;
+                    }
+                }
+                if(a.dx===1 && a.dy===1 && a.y===0) {
+                    return true;
+                }else {
+                    return false;
+                }
+            }
+            case "King" : {
+                if(Board.playerColor === Color.Red) {
+                    if(endY > 3) {
+                        return false;
+                    }
+                }else {
+                    if(endY < 8) {
+                        return false;
+                    }
+                }
+                if(a.dx===1 && a.dy===0) {
+                    return true;
+                }else if(a.dx===0 && a.dy===1) {
+                    return true;
+                }else {
+                    return false;
+                }
+            }
+            case "Gun" : {
+                let p:Piece;
+                for(let v in Board.pieceMap){
+                    if(Board.pieceMap[v].pointX === endX && Board.pieceMap[v].pointY === endY) {
+                        p = Board.pieceMap[v];
+                    }
+                }
+                if(p) {
+                    if(startX===endX && a.y === 1) {
+                        return true;
+                    }else if(startY===endY && a.x === 1) {
+                        return true;
+                    }else {
+                        return false;
+                    }
+                }else {
+                    if(startX===endX && a.y === 0) {
+                    return true;
+                    }else if(startY===endY && a.x === 0) {
+                        return true;
+                    }else {
+                        return false;
+                    }
+                }
+            }
+            case "Pawn" : {
+                if(Board.playerColor === Color.Red) {
+                    if(endY < startY) {
+                        return false;
+                    }
+                }else {
+                    if(endY > startY) {
+                        return false;
+                    }
+                }
+                if(a.dx===1 && a.dy===0) {
+                    if(Board.playerColor === Color.Red) {
+                        if(endY <= 5) {
+                            return false;
+                        }
+                    }else {
+                        if(endY >= 6) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }else if(a.dx===0 && a.dy===1) {
+                    return true;
+                }else {
+                    return false;
+                }
+            }
+        }
+    }
+
+    private is_line(startX:number,startY:number,endX:number,endY:number):boolean {
+        if(startX===endX || startY===endY) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    private is_knight(startX:number,startY:number,endX:number,endY:number):boolean {
+        if(startX===endX || startY===endY) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //判断2个棋子之间棋子数
+    private pieceCount(startX:number,startY:number,endX:number,endY:number):{x:number,y:number,z:number,dx:number,dy:number,dz:number} {
+        let a = {x:0,y:0,z:0,dx:Math.abs(startX-endX),dy:Math.abs(startY-endY),dz:0};
+        for(let i:number=Math.min(startX,endX)+1; i<Math.max(startX,endX); i++) {
+            for(let v in Board.pieceMap){
+                if(Board.pieceMap[v].pointX === i && Board.pieceMap[v].pointY === startY) {
+                    a.x++;
+                }
+            }
+        }
+        for(let i:number=Math.min(startY,endY)+1; i<Math.max(startY,endY); i++) {
+            for(let v in Board.pieceMap){
+                if(Board.pieceMap[v].pointY === i && Board.pieceMap[v].pointX === startX) {
+                    a.y++;
+                }
+            }
+        }
+        for(let i:number=Math.min(startX,endX)+1, j:number=Math.min(startY,endY)+1; i<Math.max(startX,endX), j<Math.max(startY,endY); i++,j++) {
+            a.dz++;
+            for(let v in Board.pieceMap){
+                if(Board.pieceMap[v].pointX === i && Board.pieceMap[v].pointY === j) {
+                    a.z++;
+                }
+            }
+        }
+        return a;
+    }
+    
+}
